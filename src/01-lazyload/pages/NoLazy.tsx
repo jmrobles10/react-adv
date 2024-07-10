90edd236b8265fc1f7fff1613b05f9bc70ca03e2
jmrobles10 <jm.robles30@hotmail.com>
2024-07-10 12:17:51 -0500

    Rutas anidadas

---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,11 @@
+export const NoLazy = () => {
+    return (
+        <div>
+            <h1>
+                No LayzLoad Component
+            </h1>
+        </div>
+    )
+}
+
+export default NoLazy;
